f5bcc704b4c88636f430027b19392cc6c90d9b28
Alex Frasca <alexander.frasca@student.swic.edu>
2025-02-26 23:29:52 -0600

    Add string and formatting

---
+++ src/strings.js
@@ -0,0 +1,10 @@
+const name = "Alex Frasca";
+const course = "CIS-177 Web Development";
+const grade = 85;
+
+const trad =
+	name + " is taking " + course + " and expects to earn " + grade + "%.";
+const temp = `${name} is taking ${course} and expects to earn ${grade}%.`;
+
+console.log(trad);
+console.log(temp);
